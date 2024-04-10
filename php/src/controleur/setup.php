21ca774fd502b40578152c241223bd36d7667443
HawKen147 <112583131+HawKen147@users.noreply.github.com>
2024-04-10 16:12:24 +0200

    Update setup.php

--- php/src/controleur/setup.php
+++ php/src/controleur/setup.php
@@ -16,7 +16,8 @@ function ajoute_joueurs($dbhost, $dbuser,$dbpass, $dbname){
         $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $joueur1 = "test";
+        $test = "test";
+        $joueur1 = "joueur1";
         $joueur2 = "joueur2";
         $joueur3 = "joueur3";
         $joueur4 = "joueur4";
@@ -39,6 +40,8 @@ function ajoute_joueurs($dbhost, $dbuser,$dbpass, $dbname){
         $stmt->bindParam(8, $joueur4);
         $stmt->bindParam(9, $joueur5);
         $stmt->bindParam(10, $joueur5);
+        $stmt->bindParam(11, $test);
+        $stmt->bindParam(12, $test);
     
         // Exécution de la requête
         $stmt->execute();
@@ -135,4 +138,55 @@ function lie_joueur_equipe($dbhost, $dbuser,$dbpass, $dbname){
     } catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
+}
+
+
+
+function create_tournament($dbhost, $dbuser,$dbpass, $dbname){
+    
+    try {
+        $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8", $dbuser, $dbpass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        // Requête SQL avec des paramètres
+        $requete = "INSERT INTO `tournois` (`id_tournois`, `nom_tournois`, `description`, `nombre_player`, `mode`, `image`, `link_serv`, `time_tournament`, `createur`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        // Préparation de la requête
+        $stmt = $pdo->prepare($requete);
+    
+        // Paramètres
+        $id_tournois = NULL;
+        $nom_tournois = "test tournament";
+        $description = "this is the description of the tournament";
+        $nombre_player = 2;
+        $mode = "combo";
+        $image = NULL;
+        $link_serv = NULL;
+        $time_tournament = "10/04/2026 20:00";
+        $createur = "test";
+
+
+    
+        // Liaison des valeurs aux paramètres
+        $stmt->bindParam(1, $id_tournois);
+        $stmt->bindParam(2, $nom_tournois);
+        $stmt->bindParam(3, $description);
+        $stmt->bindParam(4, $nombre_player);
+        $stmt->bindParam(5, $mode);
+        $stmt->bindParam(6, $image);
+        $stmt->bindParam(7, $link_serv);
+        $stmt->bindParam(8, $time_tournament);
+        $stmt->bindParam(9, $createur);
+    
+        // Exécution de la requête
+        $stmt->execute();
+    
+        echo "Requête exécutée avec succès.";
+    } catch (PDOException $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
+}
+
+function ajoute_equipes_tournament($dbhost, $dbuser,$dbpass, $dbname){
+    
 }
